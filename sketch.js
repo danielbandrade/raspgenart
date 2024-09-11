862f26a059f3c4c9da56e01b27156ef0ae8a29bd
root <danielbarretoandrade@gmail.com>
2024-09-11 09:08:21 -0300

    understanding better the code

--- sketch.js
+++ sketch.js
@@ -14,17 +14,12 @@ function setup() {
 
 // Spring Class
 
-function draw() {
-  
-  function Spring(_x, _y, _s, _d, _m, _k_in, _others, _id){
+function Spring(_x, _y, _s, _d, _m, _k_in, _others, _id){
     
     this.x_pos = _x;
     this.y_pos = _y;
     
-    this.size = 20;
     this.size = _s;
-    
-    this.move = true;
     
     this.mass = _m;
     this.k = _k_in;
@@ -32,7 +27,7 @@ function draw() {
     this.rest_posx = _x;
     this.rest_posy = _y;
     
-    this.velx = 0;
+    this.velx = 5;
     this.vely = 0;
     this.accel = 0;
     this.force = 0;
@@ -43,7 +38,7 @@ function draw() {
     this.update = function() {
       
         
-        this.rest_posy = mouseY;
+        this.rest_posy = this.rest_posy ;
         this.rest_posx = mouseX;
         
         this.force = - this.k * (this.y_pos - this.rest_posy);
@@ -63,31 +58,19 @@ function draw() {
       this.display = function(){
           noStroke();
 
-          rect(this.x_pos,this.y_pos,this.size,this.size);
+          ellipse(this.x_pos,this.y_pos,this.size,this.size);
           
           }
           
     
     }
+
+
+function draw() {
   
   
-  
- 
   
   background('#b20a2c');
-
-  /* // Draw the top line.
-  line(0, height/3, mouseX, width/3);
-	line(2*width/3, height, 2*width/3 , mouseY);
-
-
-  // Remap mouseX from [0, 100] to [0, 50].
-  let x = map(mouseX, 0, 100, 0, 50);
-	let y = map(mouseY, 0, 100, 0, 50)
-
-  // Draw the bottom line.
-  line(0, 2*height/3, x, 2*width/3);
-	line(width/3, height, width/3, y  + height/2 ); */
   
   springs[0] = new Spring(300,300,40,0.98,8.0,0.1,springs,0);
   
@@ -95,6 +78,12 @@ function draw() {
   
   springs[0].display();
   
- 
+  springs[1] = new Spring(550,550,40,0.98,8.0,0.1,springs,0);
+  
+  springs[1].update();
+  
+  springs[1].display();
+  
+  
   
 }
