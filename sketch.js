45835a8bbbf31d400a8797a782866dae9cc45969
root <danielbarretoandrade@gmail.com>
2024-09-14 20:29:14 -0300

    ajuste no eixo by arturito

--- sketch.js
+++ sketch.js
@@ -20,13 +20,11 @@ function Spring(_x, _y, _s, _d, _m, _k_in, _others, _id){
     this.y_pos = _y;
     
     this.size = _s;
-    
     this.mass = _m;
     this.k = _k_in;
     this.damp = _d;
     this.rest_posx = 0;
     this.rest_posy = 0;
-    
     this.velx = 0;
     this.vely = 0;
     this.accel = 0;
@@ -48,7 +46,7 @@ function Spring(_x, _y, _s, _d, _m, _k_in, _others, _id){
         
         this.force = - this.k * (this.x_pos - this.rest_posx);
         this.accel = this.force / this.mass ;
-        this.vely = this.damp * (this.velx + this.accel);
+        this.velx = this.damp * (this.velx + this.accel);
         this.x_pos = this.x_pos + this.velx;
       
       
@@ -56,6 +54,7 @@ function Spring(_x, _y, _s, _d, _m, _k_in, _others, _id){
       
       
       this.display = function(){
+        
           noStroke();
 
           ellipse(this.x_pos,this.y_pos,this.size,this.size);
@@ -70,13 +69,13 @@ function draw() {
   
   background('#b20a2c');
   
-  springs[0] = new Spring(300,300,75,0.98,8.0,0.1,springs,0);
+  springs[0] = new Spring(300,300,75,0.98,5.0,0.1,springs,0);
   
   springs[0].update();
   
   springs[0].display();
   
-  springs[1] = new Spring(550,550,75,0.98,5.0,0.5,springs,1);
+  springs[1] = new Spring(550,550,75,0.98,1.0,0.5,springs,1);
   
   springs[1].update();
   
